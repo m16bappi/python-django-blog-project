1d8a6b534aa07ccf7b9fd875f9b1e7f85d89cb44
Mehedi Hasan <mehedibubt34@gmail.com>
2021-04-25 02:04:50 +0600

    part-4

--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
+data = [
+    {
+        "name": "Mehedi",
+        "age": 23
+    },
+    {
+        "name": "Munna",
+        "age": 25
+    },
+    {
+        "name": "Shamim",
+        "age": 22
+    },
+    {
+        "name": "Ador",
+        "age": 26
+    }
+]
+
 
 def Home(request):
-    return render(request, "Home.html", {})
+    return render(request, "Home.html", {"data": data})
+
 
 def About(request):
     return render(request, "About.html", {})
